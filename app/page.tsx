376d9db05e7fd440dd9220cb7a38924215d91055
googlifytechnologies <googlifytechnologies@gmail.com>
2026-01-11 19:59:25 +0530

    Update Instagram link in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -558,7 +558,7 @@ return (
 
   {/* INSTAGRAM */}
   <div className="flex flex-col items-center space-y-2">
-    <a href="https://www.instagram.com/googlifytrchnologies/" target="_blank" className="text-gray-300 hover:text-pink-500 transition-colors">
+    <a href="https://www.instagram.com/googlifytechnologies/" target="_blank" className="text-gray-300 hover:text-pink-500 transition-colors">
       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-7 h-7">
         <path d="M7 2C4.24 2 2 4.24 2 7v10c0 2.76 2.24 5 5 5h10c2.76 0 5-2.24 5-5V7c0-2.76-2.24-5-5-5H7zm10 2c1.66 0 3 1.34 3 3v10c0 1.66-1.34 3-3 3H7c-1.66 0-3-1.34-3-3V7c0-1.66 1.34-3 3-3h10zm-5 3a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6zm4.5-2.75a1.25 1.25 0 100 2.5 1.25 1.25 0 000-2.5z"/>
       </svg>
