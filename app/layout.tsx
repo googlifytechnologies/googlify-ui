dffebe7ad356d5f3cab05ccd03d7a635e27dc2c5
Neeraj Kumar Kushwaha <neerajkushwaha660@gmail.com>
2026-01-12 14:09:37 +0530

    added og image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.png",
+        url: "https://googlifytechnologies.in/og-image.png",
         width: 1200,
         height: 630,
         alt: "Googlify Technologies",
